f7164d6ab14e62af8601070d6c21db15bd2307cf
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-01-28 10:03:42 -0600

    add js and complate p-5

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,40 @@ var createElement = function (tagName, className, text) {
 
   return element;
 };
+
+var elCountOutput = $_('.count');
+var elLowerBtn = $_('.js-lower-btn');
+var elAddBtn = $_('.js-add-btn');
+
+
+var count = Number(elCountOutput.textContent);
+
+
+function addCount() {
+  elCountOutput.textContent = ++count;
+  if(Number(elCountOutput.textContent) < 0) {
+    elCountOutput.classList.add('text-danger');
+  }else if(Number(elCountOutput.textContent) > 0){
+    elCountOutput.classList.remove('text-danger');
+    elCountOutput.classList.add('text-success');
+  }else if(Number(elCountOutput.textContent) === 0){
+    elCountOutput.classList.remove('text-success');
+    elCountOutput.classList.remove('text-danger');
+  }
+}
+
+function lowerCount() {
+  elCountOutput.textContent = --count;
+  if(Number(elCountOutput.textContent) > 0) {
+    elCountOutput.classList.add('text-success');
+  }else if(Number(elCountOutput.textContent) < 0){
+    elCountOutput.classList.add('text-danger');
+  }else if(Number(elCountOutput.textContent) === 0){
+    elCountOutput.classList.remove('text-success');
+    elCountOutput.classList.remove('text-danger');
+  }
+}
+
+
+elLowerBtn.addEventListener('click', lowerCount);
+elAddBtn.addEventListener('click', addCount);
